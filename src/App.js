7e9398b0e7f1b6d78dd9a24a4b39ed9fb7e383d6
DESKTOP-G3G8GKG\psy <sooy8951@gmail.com>
2021-10-28 02:16:20 +0900

    add

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Favorites from './components/Favorites';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <Favorites />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Favorites/style.js
@@ -0,0 +1,110 @@
+import styled from "styled-components";
+import {IoChevronBackSharp} from "react-icons/io5"
+
+export const BackButton = styled(IoChevronBackSharp)`
+    width: 30px;
+    height: 40px;
+    display: block;
+    margin: 20px 0px 5px 20px;
+
+`
+export const Header = styled.div`
+    border-bottom: 2px solid  lightgrey;
+    margin: 0px 20px 0 20px;
+    display:block;
+    h1{
+        font-size: 1.3em;
+        font-weight:bold;
+        text-align: left;
+        padding: 0px 0px 10px 20px;
+    }
+    `
+export const StoreContainer = styled.div`
+    padding:20px 20px 20px 20px;
+    border-bottom: 1px solid lightgrey;
+`;
+
+export const StoreName = styled.div`
+    font-size: 1em;
+    display: inline;
+
+`
+
+export const TextWrapper = styled.div`
+    display: inline-block;
+    width:220px;
+    padding-left:15px;
+    vertical-align: top;
+    height: 110px;
+    line-height:1.6em;
+    position: absolute;
+
+    h1{
+        font-size: 1.2em;
+        font-weight: 700;
+        margin:0px;
+    }
+    h2{
+        font-size: 1em;
+        color:grey;
+        margin: 0px;
+        font-weight: 400;
+    }
+    h3{
+        position: absolute;
+        bottom:10px;
+        right:15px;
+        font-weight: 700;
+        font-size: 1.3em;
+        margin:0px;
+    }
+
+    h4{
+        color: white;
+        width:95px;
+        font-size: 14px;
+        text-align:center;
+        border-radius: 4px; 
+        display: inline-block;
+        background-color: #379FFF;
+        margin-top:0px;
+        margin-bottom:5px;
+        margin-right:5px;
+    }
+    
+    h5{
+        color: white;
+        width:95px;
+        font-size: 14px;
+        text-align:center;
+        display: inline-block;
+        border-radius: 4px; 
+        background-color: #FF974D;
+        margin-top:0px;
+        margin-bottom:5px;
+        margin-right:5px;
+    }
+
+`
+
+export const ImageWrapper = styled.div`
+    text-align: left;
+    display: block;        
+    img{
+        height:115px;
+        width: 115px;
+        object-fit: cover;
+        border-radius: 5px;
+        vertical-align:top;
+    }
+   
+`
+
+export const Container = styled.div`
+    background-color: white;
+    padding: 5px;
+    @media (max-width: 375px){
+        width:100%;
+    }
+    width: 375px;
+`
