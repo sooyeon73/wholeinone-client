baef43c5b3ad3cb562ae1ead8b774c11ad00ead4
DESKTOP-G3G8GKG\psy <sooy8951@gmail.com>
2022-05-13 02:20:06 +0900

    fix: 로그인 에러문구 오류수정

--- src/components/LoginMenu/index.js
+++ src/components/LoginMenu/index.js
@@ -46,28 +46,26 @@ const LoginMenu = ({history}) =>{
     }
     
     const onLoginSuccess = response => {
-     
         console.log(response);
-
+        const code = response.data.code;
+        if(code==2000 || code==3000 || code==3010){
+            setErr0("로그인에 실패했습니다.");
+        }
+        else if (code==2030){
+            setErr0("비밀번호가 틀렸습니다.");
+        }
+        else
+            {
                 const  accessToken  = response.data.result.jwt;
                 const jwtValidity = response.data.result.jwtValidity;
-
                 
                 // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
                 axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;    
                 setTimeout(onSilentRefresh, jwtValidity * 1000 - 60000);
-  // accessToken 만료하기 1분 전에 로그인 연장
-  
-                const code = response.data.code;
-                if(code==2000 || code==3000 || code==3010){
-                    setErr0("로그인에 실패했습니다.");
-                }
-                else if (code==2030){
-                    setErr0("비밀번호가 틀렸습니다. 비밀번호를 다시 입력해주세요.");
-                }
-                else
-                    return true;
+                 // accessToken 만료하기 1분 전에 로그인 연장
 
+                 return true;
+            }
       }
       
     
